4922a78ed600c9a133ad463cb52427a00a04eb66
garrethil <garrethildebrandt@gmail.com>
2023-12-04 02:36:01 -0500

    set up start screen

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+const mainDiv = document.querySelector('#mainCard');
+const header = document.querySelector('#mainText');
+
+
+
+var init = function(){
+    var startText = document.createElement('h1');
+    startText.textContent = 'Click to Begin Code Quiz';
+    mainDiv.appendChild(startText);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+init();
